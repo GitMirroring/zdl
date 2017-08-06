7750869c5c75a0c8bb9cc9d59b4d82ff2a24dce3
zoninoz <zoninoz@inventati.org>
2017-08-06 12:16:09 +0200

    cinemalibero

--- extensions/000-cinemalibero.sh
+++ extensions/000-cinemalibero.sh
@@ -31,12 +31,12 @@ if [[ "$url_in" =~ cinemalibero ]]
 then
     cinemalibero_url=$(get_location "$url_in")
 
-    if ! replace_url_in "$cinemalibero_url"
-    then
-	cinemalibero_url=$(curl -s "$url_in" |
-				  grep refresh |
-				  sed -r 's|.+url=([^ "]+).*|\1|g')
-	replace_url_in "$cinemalibero_url" 
-    fi
+    replace_url_in "$cinemalibero_url"
+    
+    cinemalibero_url=$(curl -s "$url_in" |
+			      grep refresh |
+			      sed -r 's|.+url=([^ "]+).*|\1|g')
+    replace_url_in "$cinemalibero_url" 
+    
     unset cinemalibero_url
 fi
