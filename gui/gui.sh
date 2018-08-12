1528c76eea023dbfffbdcb784cf43699b8d4e58d
Gianluca Zoni <zoninoz@inventati.org>
2018-08-12 23:39:09 +0200

    gui

--- gui/gui.sh
+++ gui/gui.sh
@@ -1056,21 +1056,24 @@ function display_console_gui {
     then
 	declare -n ref="$1"
     fi
-    
-    tail -f "$gui_log" </dev/null |
-	yad --title="Console" \
-	    --image="$IMAGE2" \
-	    --text="${TEXT}\n\nConsole dei processi di estrazione e download\n\n" \
-	    --text-info \
-	    --show-uri \
-	    --uri-color=blue \
-	    --listen \
-	    --tail \
-	    --filename="$gui_log" \
-	    "${YAD_ZDL[@]}" \
-	    --button="Pulisci!gtk-refresh":"bash -c \"echo -e '\f' >'$gui_log'\"" \
-	    --button="Chiudi!gtk-ok:0" \
-	    --width=800 --height=600 2>/dev/null &
+
+    {
+	tail -f "$gui_log" </dev/null |
+	    yad --title="Console" \
+		--image="$IMAGE2" \
+		--text="${TEXT}\n\nConsole dei processi di estrazione e download\n\n" \
+		--text-info \
+		--show-uri \
+		--uri-color=blue \
+		--listen \
+		--tail \
+		--filename="$gui_log" \
+		"${YAD_ZDL[@]}" \
+		--button="Pulisci!gtk-refresh":"bash -c \"echo -e '\f' >'$gui_log'\"" \
+		--button="Chiudi!gtk-ok:0" \
+		--width=800 --height=600 2>/dev/null
+    } &
+
     if [ -n "$1" ]
     then
 	ref="$!"
