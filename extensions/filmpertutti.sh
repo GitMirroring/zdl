d6d9b841a1144f03832b6f1973e6e59451f62e88
Gianluca Zoni <zoninoz@inventati.org>
2019-03-03 15:09:11 +0100

    estrattore dei link speedvideo da filmpertutti: le pagine web nascondono i link di speedvideo con player javascript embedded

---
+++ extensions/filmpertutti.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#
+# ZigzagDownLoader (ZDL)
+# 
+# This program is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License as published 
+# by the Free Software Foundation; either version 3 of the License, 
+# or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, 
+# but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License 
+# along with this program. If not, see http://www.gnu.org/licenses/. 
+# 
+# Copyright (C) 2011: Gianluca Zoni (zoninoz) <zoninoz@inventati.org>
+# 
+# For information or to collaborate on the project:
+# https://savannah.nongnu.org/projects/zdl
+# 
+# Gianluca Zoni (author)
+# http://inventati.org/zoninoz
+# zoninoz@inventati.org
+#
+
+## zdl-extension types: shortlinks
+## zdl-extension name: filmpertutti
+
+if [[ "$url_in" =~ filmpertutti\. ]]
+then
+    urls_filmpertutti=( $(curl -s "$url_in" |
+			      grep -oP '[^"]+speedvideo[^"]+embed[^"]+') )
+    for url_fpt in "${urls_filmpertutti[@]}"
+    do
+	if url "$url_fpt"
+	then
+	    set_link + "$url_fpt"
+	    print_c 4 "Redirezione: $url_fpt"
+	fi
+    done
+    
+    if url "${urls_filmpertutti[0]}"
+    then
+	set_link - "$url_in"
+	url_in="${urls_filmpertutti[0]}"
+	print_c 4 "Nuovo link da processare: ${urls_filmpertutti[0]}"
+
+    else
+	end_extension
+    fi    
+fi
