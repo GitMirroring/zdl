92f5c581032ed6e2b10a1de95198a7c3a7fc25a2
Gianluca Zoni <zoninoz@inventati.org>
2019-05-27 19:21:04 +0200

    live

--- extensions/la7.sh
+++ extensions/la7.sh
@@ -50,7 +50,7 @@ then
 	la7_start_time="${la7_start_time//\:tomorrow}"
 	
 	get_livestream_duration_time "$url_in" la7_duration_time
-	file_in+=_$(date +%Y-%m-%d)_dalle_$(date +%H-%M-%S)__prog_inizio_${la7_start_time//\:/-}\)_durata_${la7_duration_time//\:/-}
+	file_in+=_$(date +%Y-%m-%d)_dalle_$(date +%H-%M-%S)__prog_inizio_${la7_start_time//\:/-}_durata_${la7_duration_time//\:/-}
 	
 	print_c 4 "Diretta La7 dalle $la7_start_time per la durata di $la7_duration_time"
 	livestream_m3u8="$url_in_file"
