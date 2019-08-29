43811355d781323f2c049a63dadd9e578954f78f
Gianluca Zoni <zoninoz@inventati.org>
2019-08-29 18:02:44 +0200

    traduzione gettext di log.sh

--- libs/log.sh
+++ libs/log.sh
@@ -27,7 +27,7 @@
 function init_log {
     if [ -z "$log" ]
     then
-	echo "File log di $name_prog:" > $file_log #echo "File log di $name_prog:" > $file_log
+	echo "$(eval_gettext "\$name_prog file log"):" > $file_log
 	log=1
     fi
     echo >> $file_log
@@ -43,187 +43,188 @@ function _log {
     
     case $1 in
 	1)
-	    msg="File$msg_file_in già presente in $PWD: $url_in non verrà processato."
+	    msg=$(eval_gettext "File\$msg_file_in already present in \$PWD: \$url_in will not be processed.")
 	    set_link - "${url_in}"
 	    ;;
 	2)
 	    [ -n "$errMsg" ] &&
 		errMsg=":
 $errMsg"
-	    msg="$url_in --> File$msg_file_in non disponibile, riprovo più tardi${errMsg}"
+	    msg="$url_in --> $(eval_gettext "File\$msg_file_in not available, try again later \${errMsg}")"
 	    [ -z "$file_in" ] && msg+="
-Manca il nome del file"
+$(gettext "The file name is missing")"
+
 	    [ -n "$url_in_file" ] && msg_url_in_file=": $url_in_file"
 	    url "$url_in_file" || msg+="
-Non è stato trovato un url valido$msg_url_in_file"
+$(gettext "No valid url was found")$msg_url_in_file"
+
 	    ;;
 	3)
-	    msg="$url_in --> Indirizzo errato o file non disponibile" 
+	    msg="$url_in --> $(gettext "Incorrect link or file not available")" 
 	    set_link - "$url_in"
 	    rm -f "$path_tmp"/"$file_in"_stdout.* "$path_tmp"/filename_"$file_in".txt
 	    ;;
 	4)
-	    msg="Il file$msg_file_in supera la dimensione consentita dal server per il download gratuito (link: $url_in)"
+	    msg="$(eval_gettext "The\$msg_file_in file exceeds the size allowed by the server for free download") (link: $url_in)"
 	    set_link - "$url_in"
 	    ;;
 	5)
-	    msg="Connessione interrotta: riprovo più tardi"
+	    msg="$(gettext "Connection interrupted: try again later")"
 	    ;;
 	6)
-	    msg="$url_in --> File$msg_file_in troppo grande per lo spazio libero in $PWD su $dev"
+	    msg="$url_in --> $(eval_gettext "File\$msg_file_in too large for free space in \$PWD on \$dev")"
 	    print_c 3 "$msg"
 	    echo "$msg" >> $file_log
 	    exit
 	    ;;
 	7)
-	    msg="$url_in --> File$msg_file_in già in download (${url_out[$i]})"
+	    msg="$url_in --> $(eval_gettext "File\$msg_file_in already in download") (${url_out[$i]})"
 	    ;;
 	8)
-	    msg="$url_in --> Indirizzo errato o file non disponibile.
-Errore nello scaricare la pagina HTML del video. Controllare che l'URL sia stato inserito correttamente o che il video non sia privato."
+	    msg="$url_in --> $(gettext "Incorrect link or file not available")
+$(gettext "Error downloading the video HTML page. Check that the URL has been entered correctly or that the video is not private.")"
 	    set_link - "$url_in"
 	    ;;
 	9)
-	    msg="$url_in --> Titolo della pagina HTML non trovato. Controlla l'URL."
+	    msg="$url_in --> $(gettext "Title of HTML page not found. Check the URL")."
 	    set_link - "$url_in"
 	    ;;
 	10)
-	    msg="$url_in --> Firma del video non trovata"
+	    msg="$url_in --> $(gettext "Signature of video not found")"
 	    ;;
 	11)
-	    msg="$url_in --> File$msg_file_in scaricabile solo da utenti \"Premium\" o registrati"
+	    msg="$url_in --> $(eval_gettext "File\$msg_file_in downloadable only by users \"Premium\" or registered")"
 	    set_link - "$url_in"
 	    ;;
 	12)
 	    if [ -n "$2" ]
 	    then
-		msg="$2 --> Non è un URL adatto per $name_prog
+		msg="$2 --> $(eval_gettext "It is not a valid URL for \$name_prog")
 "
 	    else
-		msg="URL non trovato"
+		msg=$(gettext "URL not found")
 	    fi
 	    set_link - "$2"
 	    ;;
 	13)
-	    msg="$url_in --> Il file$msg_file_in non sarà scaricato, perché corrisponde alla regex: $no_file_regex"
+	    msg="$url_in --> $(eval_gettext "The\$msg_file_in file will not be downloaded, because it matches to the regex"): $no_file_regex"
 	    ;;
 	14)
-	    msg="$url_in --> Il file$msg_file_in non sarà scaricato, perché non corrisponde alla regex: $file_regex"
+	    msg="$url_in --> $(eval_gettext "The\$msg_file_in file will not be downloaded, because it does not match the regex"): $file_regex"
 	    set_link - "$url_in"
 	    ;;
 	15)
-	    msg="$url_in --> Il link non sarà processato, perché corrisponde alla regex: $no_url_regex"
+	    msg="$url_in --> $(gettext "The link will not be processed, because it matches to the regex"): $no_url_regex"
 	    set_link - "$url_in"
 	    ;;
 	16)
-	    msg="$url_in --> Il link non sarà processato, perché non corrisponde alla regex: $file_regex"
+	    msg="$url_in --> $(gettext "The link will not be processed, because it does not matche to the regex"): $file_regex"
 	    set_link - "$url_in"
 	    ;;
 	17)
-	    msg="$url_in --> File$msg_file_in ancora in trasferimento e non ancora disponibile: riprova fra qualche ora" 
+	    msg="$url_in --> $(eval_gettext "File\$msg_file_in still in transfer and not yet available: try again in a few hours")"
 	    set_link - "$url_in"
 	    ;;
 	18)
-	    msg="$url_in --> resume non supportato: il download del file$msg_file_in potrebbe terminare incompleto"
+	    msg="$url_in --> $(eval_gettext "Unsupported resume: download of the\$msg_file_in file may end incomplete")"
 	    ;;
 	19)
-	    msg="$url_in --> Download non supportato: controllo età utente" 
+	    msg="$url_in --> $(gettext "Unsupported download: user age control")"
 	    set_link - "$url_in"
 	    ;;
 	20)
-	    msg="$url_in --> Download non supportato: installa lo script youtube-dl"
+	    msg="$url_in --> $(gettext "Unsupported download: install") youtube-dl"
 	    set_link - "$url_in"
 	    ;;
 	21)
-	    msg="$url_in --> Download supportato da youtube-dl, avviato ma non gestito da $PROG"
+	    msg="$url_in --> $(eval_gettext "Download supported by youtube-dl, started but not managed by \$PROG")"
 	    set_link - "$url_in"
 	    ;;
 	22)
-	    # msg="Il file ${fprefix%__M3U8__}.ts non può essere ricostruito perché incompleto:\nmanca almeno il segmento $i"
-	    msg="Manca il segmento $i: tentativo di recupero con Wget estraendo l'URL da un file temporaneo"
+	    msg=$(eval_gettext "The \$i segment is missing: recovery attempt with Wget extracting the URL from a temporary file")
 	    unset break_loop
 	    ;;
 	23)
-	    msg="Operazione non riuscita perché $dep non è installato"
+	    msg=$(eval_gettext "Operation failed because \$dep is not installed")
 	    ;;
 	
 	24)
-	    msg="Impossibile completare l'operazione: manca il file temporaneo per il recupero del segmento"
+	    msg=$(gettext "The operation could not be completed: the temporary file for segment recovery is missing")
 	    unset break_loop
 	    ;;
 	25)
-	    msg="Raggiunto il limite di download per il tuo indirizzo IP o account (link: $url_in): prova --proxy o --reconnect"
+	    msg=$(eval_gettext "Reached the download limit for your IP address or account (link: \$url_in): try --proxy or --reconnect")
 	    no_msg=true
 	    ;;
 	26)
-	    msg="$url_in --> Connessione al server IRC non riuscita: indirizzo errato o connessione o file non disponibili"
+	    msg="$url_in --> $(gettext "Connection to the IRC server failed: incorrect address or connection or file not available")"
 	    ;;
 	27)
 	    msg="<< $notice [link: $url_in]"
 	    ;;
 	28)
-	    msg="$url_in --> Un altro file è in scaricamento dalla stessa fonte, riprovo più tardi"
+	    msg="$url_in --> $(gettext "Another file is being downloaded from the same source, I will try again later")"
 	    ;;
 	29)
 	    msg="<< $notice [link: $url_in]"
 	    set_link - "$url_in"
 	    ;;
 	30)
-	    msg="$url_in --> File$msg_file_in non disponibile, riprova in un altro momento: lo cancello dalla coda"
+	    msg="$url_in --> $(eval_gettext "File\$msg_file_in not available, try again later: I delete it from the queue")"
 	    set_link - "$url_in"
 	    ;;
 	31)
-	    msg="Connessione internet non disponibile: uscita"
+	    msg=$(gettext "Internet connection not available: exit")
 	    ;;
 	32)
-	    msg="$url_in --> Servizio momentaneamente non supportato: richiedi un intervento degli sviluppatori"
+	    msg="$url_in --> $(gettext "Service temporarily not supported: request a developer intervention")"
 	    set_link - "$url_in"
 	    ;;
 	33)
-	    msg="$url_in --> In pausa per $(seconds_to_human $2)"
+	    msg="$url_in --> $(gettext "Paused for") $(seconds_to_human $2)"
 	    ;;
 	34)
-	    msg="Reindirizzamento: $url_in --> $2"
+	    msg="$(gettext "Redirection"): $url_in --> $2"
 	    color_code=4
 	    ;;
 	35)
-	    msg="$url_in --> Download non supportato: installa phantomjs"
+	    msg="$url_in --> $(gettext "Unsupported download: install") phantomjs"
 	    set_link - "$url_in"
 	    ;;
 	36)
-	    msg="$url_in --> Download non supportato: inserisci il codice captcha usando un browser web"
+	    msg="$url_in --> $(gettext "Unsupported download: enter the captcha code using a web browser")" #
 	    set_link - "$url_in"
 	    ;;
 	37)
-	    msg="$url_in --> Il servizio rileva l'uso di AdBlock o di un software come Kodi/XBMC/TV BOX (come ZigzagDownLoader). 
-Attualmente, ZDL non è in grado di scaricare il file richiesto, tolgo il link dalla coda."
+	    msg="$url_in --> $(gettext "The service detects the use of AdBlock or software such as Kodi/XBMC/TV BOX (like ZigzagDownLoader).") 
+$(gettext "Currently, ZDL is not able to download the requested file, I remove the link from the queue.")"
 	    set_link - "$url_in"
 	    ;;
 	38)
-	    msg="$url_in --> Servizio non supportato: usa il web browser per risolvere l'estrazione dell'URL del file"
+	    msg="$url_in --> $(gettext "Unsupported service: use the web browser to resolve the extraction of the file URL")"
 	    set_link - "$url_in"
 	    ;;
 	39)
-	    msg="$url_in --> Il tuo indirizzo IP è stato bannato dal server: impossibile raggiungere il file attraverso questo link" 
+	    msg="$url_in --> $(gettext "Your IP address has been banned by the server: the file cannot be reached via this link")" 
 	    set_link - "$url_in"
 	    ;;
 	40)
-	    msg="Impossibile avviare la GUI: è necessario installare yad"
+	    msg=$(gettext "Unable to start the GUI: yad must be installed")
 	    ;;
 	41)
-	    msg="$url_in --> FFMpeg/AvConv non è configurato per libxml2" 
+	    msg="$url_in --> $(gettext "FFMpeg/AvConv is not configured for libxml2")" 
 	    set_link - "$url_in"
 	    ;;
 	42)
-	    msg="$url_in --> FFMpeg/AvConv non è installato" 
+	    msg="$url_in --> $(gettext "FFMpeg/AvConv is not installed")" 
 	    set_link - "$url_in"
 	    ;;
 	43)
-	    msg="$url_in --> Durata del Live Stream non registrata: ripetere la programmazione (il link è cancellato dalla coda)" 
+	    msg="$url_in --> $(gettext "Duration of the unrecorded Live Stream: repeat the programming (the link is deleted from the queue)")" 
 	    set_link - "$url_in"
 	    ;;
 	44)
-	    msg="$url_in --> attesa imposta a utenti non premium, riprovo più tardi"
+	    msg="$url_in --> $(gettext "Wait set to non-premium users, try again later")" 
 	    no_filelog=true
 	    ;;
     esac
