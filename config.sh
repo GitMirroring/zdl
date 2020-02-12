9e328d799924557960e198821af9e986f12babe0
Gianluca Zoni <zoninoz@inventati.org>
2020-02-12 17:13:18 +0100

    awk ...

--- config.sh
+++ config.sh
@@ -52,7 +52,7 @@ function set_default_conf {
 }
 
 function get_item_conf {	
-    awk "{match(\$0,/^\ *$1=\"*([^\"]+)\"*$/,pattern); if (pattern[1]) print pattern[1]}" "$file_conf" |
+    awk "{match(\$0,/^\s*$1=\"*([^\"]+)\"*$/,pattern); if (pattern[1]) print pattern[1]}" "$file_conf" |
 	tail -n1 ## ultimo valore se nel file di configurazione sono presenti valori diversi per una stessa chiave
 }
 
