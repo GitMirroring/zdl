c48d9c0d430267e750bf1ea876f5c0698f63d211
Gianluca Zoni <zoninoz@inventati.org>
2020-05-29 18:00:14 +0200

    raiscuola

---
+++ extensions/raiscuola.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -i
+#
+# ZigzagDownLoader (ZDL)
+# 
+# This program is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License as published 
+# by the Free Software Foundation; either version 3 of the License, 
+# or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, 
+# but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License 
+# along with this program. If not, see http://www.gnu.org/licenses/. 
+# 
+# Copyright (C) 2011: Gianluca Zoni (zoninoz) <zoninoz@inventati.org>
+# 
+# For information or to collaborate on the project:
+# https://savannah.nongnu.org/projects/zdl
+# 
+# Gianluca Zoni (author)
+# http://inventati.org/zoninoz
+# zoninoz@inventati.org
+#
+## ZDL add-on
+## zdl-extension types: streaming
+## zdl-extension name: raiscuola.rai.it (HD)
+
+
+if [[ "$url_in" =~ raiscuola\.rai\.it\/articoli ]]
+then
+    html=$(curl -s "$url_in")
+    file_in=$(grep 'og:title' <<< "$html")
+    file_in="${file_in#*'og:title'}"
+    file_in="${file_in#*content=\"}"
+    file_in="${file_in%%\"*}"
+
+    url_in_file=$(grep mp4 <<< "$html")
+    url_in_file="${url_in_file##*src\: \'}"
+    url_in_file="${url_in_file%%\'*}"
+    
+    end_extension
+fi
+									   
+
