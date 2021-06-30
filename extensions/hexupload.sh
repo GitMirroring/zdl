635a2b024cc12617e16be41dc618f3c843fcab76
zoninoz <zoninoz@gmail.com>
2021-06-30 16:54:06 +0200

    hexupload

---
+++ extensions/hexupload.sh
@@ -0,0 +1,62 @@
+#!/bin/bash -i
+#
+# ZigzagDownLoader (ZDL)
+# 
+# This program is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License as published 
+# by the Free Software Foundation; either version 3 of the License, 
+# or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, 
+# but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License 
+# along with this program. If not, see http://www.gnu.org/licenses/. 
+# 
+# Copyright (C) 2011: Gianluca Zoni (zoninoz) <zoninoz@inventati.org>
+# 
+# For information or to collaborate on the project:
+# https://savannah.nongnu.org/projects/zdl
+# 
+# Gianluca Zoni (author)
+# http://inventati.org/zoninoz
+# zoninoz@inventati.org
+#
+
+
+## zdl-extension types: download
+## zdl-extension name: Exashare
+
+
+if [ "$url_in" != "${url_in//hexupload.}" ]
+then
+    html=$(wget -qO- -t 1 -T $max_waiting                \
+		--user-agent="$user_agent"               \
+		"$url_in"                                \
+		-o /dev/null)
+
+    input_hidden "$html"
+    
+    html2=$(wget -qO-                                                    \
+		 --user-agent="$user_agent"                              \
+		 --post-data="${post_data}&method_free=Free Download"    \
+		 "$url_in"                                               \
+		 -o /dev/null)
+
+    input_hidden "$html2"
+
+    post_data="${post_data%adblock_detected*}adblock_detected=0"
+    
+    html3=$(wget -qO-                          \
+		 --user-agent="$user_agent"    \
+		 --post-data="${post_data}"    \
+		 "$url_in"                     \
+		 -o /dev/null)
+
+    url_in_file=$(grep 'Click Here To Download' <<< "$html3")
+    url_in_file="${url_in_file#*href=\"}"
+    url_in_file="${url_in_file%%\"*}"
+    
+    end_extension
+fi
