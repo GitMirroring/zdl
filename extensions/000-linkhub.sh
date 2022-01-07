0bcae1a34f4a8d8e2a5c49e88754ed9fb1a94bfd
zoninoz <zoninoz@gmail.com>
2022-01-07 22:33:51 +0100

    linkhub

--- extensions/000-linkhub.sh
+++ extensions/000-linkhub.sh
@@ -30,65 +30,77 @@
 function get_linkhub {
     local link="$1" newlink html 
     declare -a links=()
-    
-    html=$(curl "$link")
-    html=$(grep get_btn <<< "$html")
-    html="${html#*href=\"}"
-    html="${html%%\"*}"
 
-    link_parser "$link"
+    for i in 0 1 2 3 4 5
+    do
+        html=$(curl "$link")
+        html=$(grep get_btn <<< "$html")
+        html="${html#*href=\"}"
+        html="${html%%\"*}"
+        linkhub_code="${html#*\/}"
+        sleep 1
+        [ -n "${linkhub_code//view\/}" ] && break
+    done
 
-    get_language
-    print_c 4 "$(gettext "Redirection"): $link --> ${parser_proto}${parser_domain}/${html#*\/}"
-    get_language_prog
-
-    html=$(curl "${parser_proto}${parser_domain}/${html#*\/}")
-
-    if [[ "$html" =~ text-url ]]
+    if [ -z "$linkhub_code" ]
     then
-	if [ -z "$newlink_first" ]
-	then
-            newlink_first=$(grep text-url -A1 <<<  "$html" |
-                                tail -n1 |
-                                sed -r 's|^[^"]+\"([^"]+)\".+|\1|' )
+        return 2
 
-            newlink_first=$(sanitize_url "$newlink_first")
-        fi
-        
-        links+=( $(grep -P 'href.+target=\"_blank\" title=\"' <<< "$html" |
-    		   sed -r 's|.+>([^<]+)<\/a>.+|\1|g') )
-        
-        for newlink in "${links[@]}"
-        do
-	    newlink=$(trim "$newlink")
-	    if url "$newlink" &&
-                    [ "$newlink" != "$url_in" ]
+    else
+        link_parser "$link"
+
+        get_language
+        print_c 4 "$(gettext "Redirection"): $link --> ${parser_proto}${parser_domain}/${linkhub_code}"
+        get_language_prog
+
+        html=$(curl "${parser_proto}${parser_domain}/${linkhub_code}")
+
+        if [[ "$html" =~ text-url ]]
+        then
+	    if [ -z "$newlink_first" ]
 	    then
-                if [[ "$newlink" =~ ninjastream\..+\/watch\/ ]]
-                then
-                    newlink="${newlink//\/watch\///download/}"
+                newlink_first=$(grep text-url -A1 <<<  "$html" |
+                                    tail -n1 |
+                                    sed -r 's|^[^"]+\"([^"]+)\".+|\1|' )
+
+                newlink_first=$(sanitize_url "$newlink_first")
+            fi
+            
+            links+=( $(grep -P 'href.+target=\"_blank\" title=\"' <<< "$html" |
+    		           sed -r 's|.+>([^<]+)<\/a>.+|\1|g') )
+            
+            for newlink in "${links[@]}"
+            do
+	        newlink=$(trim "$newlink")
+	        if url "$newlink" &&
+                        [ "$newlink" != "$url_in" ]
+	        then
+                    if [[ "$newlink" =~ ninjastream\..+\/watch\/ ]]
+                    then
+                        newlink="${newlink//\/watch\///download/}"
+                        
+                    elif [ "$newlink" != "${newlink//linkhub\.}" ]
+                    then
+                        get_linkhub "$newlink"
+                        continue
+                    fi
+
+                    if ( [ -n "$no_url_regex" ] && [[ "${newlink}" =~ $no_url_regex ]] ) ||
+                           ( [ -n "$url_regex" ] && [[ ! "${newlink}" =~ $url_regex ]] )    
+	            then
+                        [ "$newlink_first" == "$newlink" ] && unset newlink_first
+                        continue
+	            fi
+
+	            set_link + "$newlink"
                     
-                elif [ "$newlink" != "${newlink//linkhub\.}" ]
-                then
-                    get_linkhub "$newlink" || _log 36
-                    continue
-                fi
-
-                if ( [ -n "$no_url_regex" ] && [[ "${newlink}" =~ $no_url_regex ]] ) ||
-                       ( [ -n "$url_regex" ] && [[ ! "${newlink}" =~ $url_regex ]] )    
-	        then
-                    [ "$newlink_first" == "$newlink" ] && unset newlink_first
-                    continue
-	        fi
-
-	        set_link + "$newlink"
-                
-	        if [ -z "$newlink_first" ]
-	        then
-		    newlink_first="$newlink"
-	        fi
-            fi                    
-        done
+	            if [ -z "$newlink_first" ]
+	            then
+		        newlink_first="$newlink"
+	            fi
+                fi                    
+            done
+        fi
     fi
 }
  
@@ -96,7 +108,7 @@ if [ "$url_in" != "${url_in//linkhub\.}" ]
 then
     for i in 0 1 2
     do
-        get_linkhub "$url_in" || _log 36
+        get_linkhub "$url_in"
         [[ ! "$newlink_first" =~ linkhub\. ]] && break
     done
 
