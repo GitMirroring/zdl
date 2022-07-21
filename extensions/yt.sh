c76c820764455b87c6842cef0c75dc340c50e8b2
zoninoz <zoninoz@inventati.org>
2022-07-21 13:53:29 +0200

    youtube

--- extensions/yt.sh
+++ extensions/yt.sh
@@ -137,18 +137,9 @@ then
 
     if check_livestream "$url_in_file"
     then
-        # echo display
-        # yt_that_mode="$this_mode"
-        # this_mode=stdout
-        # display_set_livestream "$url_in"
-        # this_mode=$yt_that_mode
-        
         get_livestream_start_time "$url_in" yt_start_time
         get_livestream_duration_time "$url_in" yt_duration_time
-        #grep -P "^ht.+youtube.+\ [0-9]+\:" "$path_tmp"/livestream_time.txt
-        #cat "$path_tmp"/livestream_time.txt
-        echo "url: $url_in_file" 
-        echo "durata: $yt_duration_time"
+
         file_in="${file_in%.mp4}"_$(date +%Y-%m-%d)_dalle_$(date +%H-%M-%S)__prog_inizio_${yt_start_time//\:/-}_durata_${yt_duration_time//\:/-}.mp4
 
         if [ -n "$yt_duration_time" ]
