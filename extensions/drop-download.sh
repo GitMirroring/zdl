7561073df565029fadf65e9c15bc0431265f6bbc
zoninoz <zoninoz@inventati.org>
2022-08-02 22:08:09 +0200

    drop-download

--- extensions/drop-download.sh
+++ extensions/drop-download.sh
@@ -31,29 +31,32 @@
 if [[ "$url_in" =~ drop\.download ]]
 then
     get_language_prog
-    location_drop=$(curl -v \
-                         -c "$path_tmp"/cookies.zdl \
-                         "$url_in" 2>&1 |
-                        awk "/location/{print \$3}")
+    html=$(curl -v \
+                -c "$path_tmp"/cookies.zdl \
+                "$url_in" 2>&1)
+
+    location_drop=$(awk "/location/{print \$3}" <<< "$html")
     get_language
+
     location_drop=$(trim "$location_drop")
 
     if url "$location_drop"
     then
         _log 34 "$location_drop"
-
+        
         # html=$(curl -v \
-        #             -A "$user_agent" \
-        #             -b "$path_tmp"/cookies.zdl \
-        #             -c "$path_tmp"/cookies2.zdl \
-        #             -H "Connection: keep-alive" \
-        #             -H "Upgrade-Insecure-Requests: 1" \
-        #             -H "Sec-Fetch-Dest: document" \
-        #             -H "Sec-Fetch-Mode: navigate" \
-        #             -H "Sec-Fetch-Site: cross-site" \
-        #             -H "Cache-Control: max-age=0" \
-        #             -H "TE: trailers" \
-        #             "$location_drop" 2>&1)
+            #             -A "$user_agent" \
+            #             -b "$path_tmp"/cookies.zdl \
+            #             -c "$path_tmp"/cookies2.zdl \
+            #             -H "Connection: keep-alive" \
+            #             -H "Upgrade-Insecure-Requests: 1" \
+            #             -H "Sec-Fetch-Dest: document" \
+            #             -H "Sec-Fetch-Mode: navigate" \
+            #             -H "Sec-Fetch-Site: cross-site" \
+            #             -H "Cache-Control: max-age=0" \
+            #             -H "TE: trailers" \
+            #             "$location_drop" 2>&1)
+        
         html=$(curl -v \
                     -A "$user_agent" \
                     -b "$path_tmp"/cookies.zdl \
@@ -66,26 +69,29 @@ then
                     -H "Sec-Fetch-User: ?1" \
                     -H "TE: trailers" \
                     "$location_drop" 2>&1)
+    else
+        location_drop="$url_in"
+    fi
+    
+    input_hidden "$html"
+    post_data="${post_data}&method_free=Free+Download+%3E%3E"
 
-        input_hidden "$html"
-        post_data="${post_data}&method_free=Free+Download+%3E%3E"
+    html=$(curl -v \
+                -A "$user_agent" \
+                -b "$path_tmp"/cookies2.zdl \
+                -c "$path_tmp"/cookies.zdl \
+                -H "Origin: https://drop.download" \
+                -H "Connection: keep-alive" \
+                -H "Referer: $location_drop" \
+                -H "Upgrade-Insecure-Requests: 1" \
+                -H "Sec-Fetch-Dest: document" \
+                -H "Sec-Fetch-Mode: navigate" \
+                -H "Sec-Fetch-Site: same-origin" \
+                -H "Sec-Fetch-User: ?1" \
+                -d "$post_data" \
+                "$location_drop" 2>&1)
 
-        html=$(curl -v \
-                    -A "$user_agent" \
-                    -b "$path_tmp"/cookies2.zdl \
-                    -c "$path_tmp"/cookies.zdl \
-                    -H "Origin: https://drop.download" \
-                    -H "Connection: keep-alive" \
-                    -H "Referer: $location_drop" \
-                    -H "Upgrade-Insecure-Requests: 1" \
-                    -H "Sec-Fetch-Dest: document" \
-                    -H "Sec-Fetch-Mode: navigate" \
-                    -H "Sec-Fetch-Site: same-origin" \
-                    -H "Sec-Fetch-User: ?1" \
-                    -d "$post_data" \
-                    "$location_drop" 2>&1)
-
-        # html=$(wget -SO -  \
+    # html=$(wget -SO -  \
         #             --user-agent="$user_agent" \
         #             --load-cookies="$path_tmp"/cookies2.zdl \
         #             --save-cookies="$path_tmp"/cookies.zdl \
@@ -100,37 +106,36 @@ then
         #             --post-data="$post_data" \
         #             "$location_drop" 2>&1)
 
-        drop_unpacked=$(unpack "$(grep 'p,a,c,k,e' <<<  "$html" | tail -n1)")
+    drop_unpacked=$(unpack "$(grep 'p,a,c,k,e' <<<  "$html" | tail -n1)")
 
-        url_in_file="${drop_unpacked##*\<param name\=\"src\"value\=\"}"
+    url_in_file="${drop_unpacked##*\<param name\=\"src\"value\=\"}"
+    url_in_file="${url_in_file%%\"*}"
+
+    if ! url "$url_in_file"
+    then
+        captcha_code=$(pseudo_captcha "$html")
+        
+        input_hidden "$html"
+        
+        post_data="${post_data%%Free Download*}Free+Download+%3E%3E&method_premium=&adblock_detected=0&code=$captcha_code"
+        html=$(curl -v \
+                    -A "$user_agent" \
+                    -b "$path_tmp"/cookies.zdl \
+                    -c "$path_tmp"/cookies2.zdl \
+                    -H "Connection: keep-alive" \
+                    -H "Upgrade-Insecure-Requests: 1" \
+                    -H "Sec-Fetch-Dest: document" \
+                    -H "Sec-Fetch-Mode: navigate" \
+                    -H "Sec-Fetch-Site: same-origin" \
+                    -H "Sec-Fetch-User: ?1" \
+                    -H "TE: trailers" \
+                    -d "$post_data" \
+                    "$location_drop" 2>&1)
+
+        url_in_file=$(grep 'Click here to download' <<< "$html" -B1 | head -n1)
+        url_in_file="${url_in_file#*\"}"
         url_in_file="${url_in_file%%\"*}"
-
-        if ! url "$url_in_file"
-        then
-            captcha_code=$(pseudo_captcha "$html")
-            
-            input_hidden "$html"
-            
-            post_data="${post_data%%Free Download*}Free+Download+%3E%3E&method_premium=&adblock_detected=0&code=$captcha_code"
-            html=$(curl -v \
-                        -A "$user_agent" \
-                        -b "$path_tmp"/cookies.zdl \
-                        -c "$path_tmp"/cookies2.zdl \
-                        -H "Connection: keep-alive" \
-                        -H "Upgrade-Insecure-Requests: 1" \
-                        -H "Sec-Fetch-Dest: document" \
-                        -H "Sec-Fetch-Mode: navigate" \
-                        -H "Sec-Fetch-Site: same-origin" \
-                        -H "Sec-Fetch-User: ?1" \
-                        -H "TE: trailers" \
-                        -d "$post_data" \
-                        "$location_drop" 2>&1)
-
-            url_in_file=$(grep 'Click here to download' <<< "$html" -B1 | head -n1)
-            url_in_file="${url_in_file#*\"}"
-            url_in_file="${url_in_file%%\"*}"
-            
-        fi        
-    fi
+        
+    fi        
     end_extension
 fi
