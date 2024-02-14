872363abfd4488623ce76e00a98d0fbeabde253f
Gianluca Zoni <zoninoz@rizoma.zoninoz>
2024-02-14 06:32:53 +0100

    raiplay

--- extensions/raiplay.sh
+++ extensions/raiplay.sh
@@ -240,13 +240,13 @@ then
             season_number="${season_number##* }"
             if [ "${season_number}x${episode_number}" != x ]
             then
-                file_in_prefix="${season_number}x$(printf "%.2d" ${episode_number})"
+                file_in_prefix="${season_number}x$(printf "%.2d" ${episode_number})_-_"
             fi
             url_in_file="${raiplay_data_json##*\"manifest_url\": \"}"
             url_in_file="${url_in_file%%\"*}"
             
             file_in="${raiplay_data_json##*\"title\": \"}"
-            file_in="${file_in_prefix}_-_${file_in%%\"*}".mp4
+            file_in="${file_in_prefix}${file_in%%\"*}".mp4
         fi
         # if ! url "$url_in_file"
         # then
